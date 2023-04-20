621dd7490def0ab9f924d6bb1aa5f6093b549f31
Kaja Więckowska <k.wieckowska66@gmail.com>
2023-04-20 14:45:37 +0200

    Added tests to restaurant controller

---
+++ RestaurantAPI.KajaIntegrationTests/RestaurantControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace RestaurantAPI.KajaIntegrationTests
+{
+    public class RestaurantControllerTests
+    {
+        [Theory]
+        [InlineData("pageSize=5&pageNumber=1")]
+        [InlineData("pageSize=15&pageNumber=2")]
+        [InlineData("pageSize=10&pageNumber=3")]
+        public async Task GetAll_WitchQueryParameters_ReturnsOkResult(string queryParams)
+        {
+            //Arrange
+            var factory = new WebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+
+            //Act
+            var response = await client.GetAsync("/api/restaurant?" + queryParams);
+
+            //Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Theory]
+        [InlineData("pageSize=100&pageNumber=3")]
+        [InlineData("pageSize=11&pageNumber=3")]
+        [InlineData(null)]
+        [InlineData("")]
+        public async Task GetAll_WithInvalidQueryParams_ReturnsBadRequest(string queryParams)
+        {
+            //Arrange
+            var factory = new WebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+
+            //Act
+            var response = await client.GetAsync("/api/restaurant?" + queryParams);
+
+            //Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+    }
+}
