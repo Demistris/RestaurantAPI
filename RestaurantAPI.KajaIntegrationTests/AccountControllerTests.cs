4d49eb0c233f1dbc24aa896921453c30748fdc80
Kaja Więckowska <k.wieckowska66@gmail.com>
2023-05-09 09:57:39 +0200

    Testing Register User

---
+++ RestaurantAPI.KajaIntegrationTests/AccountControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantAPI.Entities;
+using RestaurantAPI.KajaIntegrationTests.Helpers;
+using RestaurantAPI.Models;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestaurantAPI.KajaIntegrationTests
+{
+    public class AccountControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private HttpClient _client;
+
+        public AccountControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var dbContextOptions = services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<RestaurantDbContext>));
+                    services.Remove(dbContextOptions);
+
+                    services.AddDbContext<RestaurantDbContext>(options => options.UseInMemoryDatabase("RestaurantDb"));
+                });
+            }).CreateClient();
+        }
+
+        [Fact]
+        public async Task RegisterUser_ForValidModel_ReturnsOk()
+        {
+            //Arrange
+            var registerUser = new RegisterUserDto()
+            {
+                Email = "test@test.com",
+                Password = "123456",
+                ConfirmPassword = "123456"
+            };
+
+            var httpContent = registerUser.ToJsonHttpContent();
+
+            //Act
+            var response = await _client.PostAsync("/api/account/register", httpContent);
+
+            //Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+        
+        [Fact]
+        public async Task RegisterUser_ForInvalidModel_ReturnsBadRequest()
+        {
+            //Arrange
+            var registerUser = new RegisterUserDto()
+            {
+                Password = "123456",
+                ConfirmPassword = "1234567"
+            };
+
+            var httpContent = registerUser.ToJsonHttpContent();
+
+            //Act
+            var response = await _client.PostAsync("/api/account/register", httpContent);
+
+            //Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+    }
+}
