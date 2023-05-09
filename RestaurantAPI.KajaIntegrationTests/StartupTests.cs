0958d0ff21916b09f45477ef49ca5998d756d50a
Kaja Więckowska <k.wieckowska66@gmail.com>
2023-05-09 15:42:19 +0200

    Dependency registration testing

---
+++ RestaurantAPI.KajaIntegrationTests/StartupTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace RestaurantAPI.KajaIntegrationTests
+{
+    public class StartupTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly List<Type> _controllerTypes;
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public StartupTests(WebApplicationFactory<Startup> factory)
+        {
+            _controllerTypes = typeof(Startup).Assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(ControllerBase))).ToList();
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    _controllerTypes.ForEach(c => services.AddScoped(c));
+                });
+            });
+        }
+
+        [Fact]
+        public void ConfigureServices_ForControllers_RegistersAllDependencies()
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+
+            //Assert
+            _controllerTypes.ForEach(t =>
+            {
+                var controller = scope.ServiceProvider.GetService(t);
+                controller.Should().NotBeNull();
+            });
+        }
+    }
+}
