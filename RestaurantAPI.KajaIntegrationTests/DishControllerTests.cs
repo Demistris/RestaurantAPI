15bec1e3e1442e07f5efadcd82e5da705b1eeb02
Kaja Więckowska <k.wieckowska66@gmail.com>
2023-05-09 20:37:03 +0200

    Added empty dish controller test

---
+++ RestaurantAPI.KajaIntegrationTests/DishControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace RestaurantAPI.KajaIntegrationTests
+{
+    public class DishControllerTests
+    {
+        [Fact]
+        public void Delete_DishFromRestaurantWithId_ReturnsSuccess()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
